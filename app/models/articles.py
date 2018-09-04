d83f03494fadd9baba329780fe879551ee1f8ac8
billowbashir <billowbashir@gmail.com>
2018-09-04 10:20:47 +0300

    make the link to articles work

--- app/models/articles.py
+++ app/models/articles.py
@@ -3,6 +3,7 @@ class Articles:
     articles class to define the article Objects
     '''
     def __init__(self,source,author,title,description,url,urlToImage,publishedAt):
+        # self.id=id
         self.source=source
         self.author=author
         self.title=title
--- app/views.py
+++ app/views.py
@@ -3,15 +3,15 @@ from app import app
 from .request import get_source,get_articles
 @app.route('/')
 def index():
-
+    # articles=get_articles(id)
     news_sources=get_source()
     print(news_sources)
     title='welcome to news highlighter'
     return render_template('index.html', title=title, sources=news_sources)
 
 
-@app.route('/articles/')
-def source():
-    articles=get_articles()
+@app.route('/articles/<string:id>')
+def source(id):
+    articles=get_articles(id)
     print(articles)
     return render_template('articles.html',articles=articles)
